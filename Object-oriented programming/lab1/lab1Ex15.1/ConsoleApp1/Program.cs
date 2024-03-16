3c1649e63163b6799d3d4fec6e8b454f1cd6e47e
Nightik <106484814+TweLftHSide@users.noreply.github.com>
2024-03-16 21:08:12 +0200

    Add files via upload

---
+++ Object-oriented programming/lab1/lab1Ex15.1/ConsoleApp1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class CountWords
+{
+    private Dictionary<string, int> _wordCounts;
+
+    public CountWords()
+    {
+        _wordCounts = new Dictionary<string, int>();
+    }
+
+    public void AddWord(string word)
+    {
+        if (_wordCounts.ContainsKey(word))
+        {
+            _wordCounts[word]++;
+        }
+        else
+        {
+            _wordCounts[word] = 1;
+        }
+    }
+
+    public int GetWordCount(string word)
+    {
+        if (_wordCounts.ContainsKey(word))
+        {
+            return _wordCounts[word];
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    public void PrintWordCounts()
+    {
+        foreach (KeyValuePair<string, int> kvp in _wordCounts)
+        {
+            Console.WriteLine("{0}: {1}", kvp.Key, kvp.Value);
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CountWords counter = new CountWords();
+
+        while (true)
+        {
+            string input = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(input))
+            {
+                break;
+            }
+
+            string[] words = input.Split(' ');
+
+            foreach (string word in words)
+            {
+                counter.AddWord(word);
+            }
+        }
+
+        counter.PrintWordCounts();
+    }
+}
+
