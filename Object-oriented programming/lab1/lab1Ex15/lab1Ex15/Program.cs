fe4ec5d17d39223026d79d8e5544797046adcce4
Nightik <106484814+TweLftHSide@users.noreply.github.com>
2024-03-16 20:14:23 +0200

    Add files via upload

---
+++ Object-oriented programming/lab1/lab1Ex15/lab1Ex15/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class CountWords
+{
+    private int _wordCount;
+    private List<string> _words;
+
+    public CountWords()
+    {
+        _wordCount = 0;
+        _words = new List<string>();
+    }
+
+    public void AddWord(string word)
+    {
+        _wordCount++;
+        _words.Add(word);
+    }
+
+    public int GetWordCount()
+    {
+        return _wordCount;
+    }
+
+    public List<string> GetWords()
+    {
+        return _words;
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CountWords counter = new CountWords();
+
+        while (true)
+        {
+            string input = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(input))
+            {
+                break;
+            }
+
+            string[] words = input.Split(' ');
+
+            foreach (string word in words)
+            {
+                counter.AddWord(word);
+            }
+        }
+
+        Console.WriteLine("Кількість слів: {0}", counter.GetWordCount());
+
+        foreach (string word in counter.GetWords())
+        {
+            Console.WriteLine(word);
+        }
+    }
+}
+
